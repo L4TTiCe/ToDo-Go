023648d928a7689b3982a4997bf0ec6e64774198
L4TTiCe <dheshan.mail@gmail.com>
2022-07-16 10:50:06 -0400

    feat: Use 'godotenv'

--- src/server.go
+++ src/server.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func main() {
+// loadEnv loads the .env file if it exists
+func loadEnv() {
+	log.Println("Loading .env file...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found")
+	}
+}
+
+// configureLogger sets optional flags for the logger
+func configureLogger() {
+	log.Print("Configuring logger...")
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+func initializeRouter() *gin.Engine {
+	log.Println("Initializing router...")
 	router := gin.Default()
 
 	router.GET("/up", func(c *gin.Context) {
 		c.String(http.StatusOK, "Server is Up!")
 	})
+
+	return router
+}
+
+func main() {
+	configureLogger()
+	loadEnv()
+	router := initializeRouter()
 
 	err := router.Run(":8080")
 	if err != nil {
